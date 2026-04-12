b0a9ba0403d163352ad726d7a965a498b4a6b125
Lucas Farnese <lrfarnese@gmail.com>
2026-04-12 15:49:11 -0300

    Adicionado Controller do Feed e rota da tela principal

---
+++ app/Http/Controllers/Feed/FeedController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Feed;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class FeedController extends Controller
+{
+    public function index(){
+        
+    
+    
+    return view('feed.feed-page');
+    }
+}
--- resources/views/feed/indexFeed.blade.php
+++
@@ -1,6 +0,0 @@
-@extends('layouts.feed')
-
-@section('content')
-      
-      
-@endsection
